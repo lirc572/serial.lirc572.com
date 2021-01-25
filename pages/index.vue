30c470d120117bac44b4567d476e4aef333533ae
lirc572 <34601854+lirc572@users.noreply.github.com>
2021-01-25 16:02:16 +0800

    feat: add console textarea, input textfield and send button

--- pages/index.vue
+++ pages/index.vue
@@ -15,70 +15,54 @@
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Serial Monitor
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
           <v-btn
+            elevation="2"
             color="primary"
-            nuxt
-            to="/inspire"
+            @click="selectPort"
           >
-            Continue
+            Select Port
           </v-btn>
-        </v-card-actions>
+          <v-btn
+            elevation="2"
+            color="success"
+            :disabled="!port"
+            @click="openPort"
+          >
+            Connect
+          </v-btn>
+          <hr class="my-3">
+          <v-textarea
+            v-show="reader"
+            id="console-textarea"
+            outlined
+            readonly
+            name="console-textarea"
+            label="Console"
+            :value="consoleText"
+          />
+          <v-row
+            v-show="reader"
+            id="send-row"
+            justify="center"
+            align="baseline"
+          >
+            <v-text-field
+              v-model="inputText"
+              label="Send Text"
+              clearable
+            />
+            <v-btn
+              elevation="2"
+              color="cyan"
+              @click="writeToOutputStream"
+            >
+              Send
+            </v-btn>
+          </v-row>
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
@@ -97,6 +81,94 @@ export default {
     return {
       serialSupported: process.browser ? ('serial' in navigator) : true
     }
+  },
+  data () {
+    return {
+      port: null,
+      inputStream: null,
+      reader: null,
+      outputStream: null,
+      consoleText: '',
+      inputText: ''
+    }
+  },
+  methods: {
+    updateConsoleText (text) {
+      this.consoleText = this.consoleText.slice(0, -1) + text + '\n'
+    },
+    async selectPort () {
+      try {
+        this.port = await navigator.serial.requestPort()
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error)
+      }
+    },
+    async openPort () {
+      try {
+        await this.port.open({ baudRate: 115200 })
+        this.createInputStream()
+        this.createOutputStream()
+        this.readLoop()
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error)
+      }
+    },
+    createInputStream () {
+      // eslint-disable-next-line no-undef
+      const decoder = new TextDecoderStream()
+      this.port.readable.pipeTo(decoder.writable)
+      this.inputStream = decoder.readable
+      this.reader = this.inputStream.getReader()
+    },
+    createOutputStream () {
+      // eslint-disable-next-line no-undef
+      const encoder = new TextEncoderStream()
+      encoder.readable.pipeTo(this.port.writable)
+      this.outputStream = encoder.writable
+    },
+    writeToOutputStream () {
+      const lines = this.inputText.split('\n')
+      const writer = this.outputStream.getWriter()
+      lines.forEach((line) => {
+        // eslint-disable-next-line no-console
+        console.log('[SEND]', line)
+        writer.write(line + '\n')
+        this.updateConsoleText(line + '\n')
+      })
+      this.inputText = ''
+      writer.releaseLock()
+    },
+    async readLoop () {
+      while (true) {
+        const { value, done } = await this.reader.read()
+        if (value) {
+          // eslint-disable-next-line no-console
+          console.log(value)
+          this.updateConsoleText(value)
+          this.consoleScrollToBottom()
+        }
+        if (done) {
+          // eslint-disable-next-line no-console
+          console.log('[readLoop] DONE', done)
+          this.reader.releaseLock()
+          break
+        }
+      }
+    },
+    consoleScrollToBottom () {
+      const consoleElement = document.getElementById('console-textarea')
+      // eslint-disable-next-line no-console
+      console.log('Scrolling to', consoleElement.scrollHeight)
+      consoleElement.scrollTop = consoleElement.scrollHeight
+    }
   }
 }
 </script>
+<style scoped>
+#send-row {
+  margin-left: 0px;
+  margin-right: 0px;
+}
+</style>
